031696eead1e7b52df5e87788f3604e575bb05ea
Luggamugga <l.ongyerth@gmail.com>
2023-02-22 02:12:33 +0100

    asdf

--- shapesApp.js
+++ shapesApp.js
@@ -12,11 +12,13 @@ let stage = new Konva.Stage({
     height: drawHeight,
 })
 
-let layer = new Konva.Layer();
+let movable = new Konva.Layer();
+let stationary = new Konva.Layer();
 //group for draggableShapes:
 let draggableShapes = new Konva.Group();
 //group for stationary shapes (or maybe we do a layer idk):
-let staticShapes = new Konva.Group();
+let staticShapes = [];
+let shapes = new Konva.Group();
 
 //getting length values from $_GET request in url:
 let tempVal = window.location.href.split("?");
@@ -32,40 +34,134 @@ let radius = (A*B*C)/(4*Math.sqrt(s*(s-A)*(s-B)*(s-C)))
 */
 //https://keisan.casio.com/exec/system/1223429573 : Formulae for radius
 
-function TriangleGen(startX,startY,stroke,fill,draggable){
-    let P = [startX,startY];
-    let Q = [startX+A,startY];
-    let R = [startX+A,startY+A]
-    let arr = findCircumCenter(P,Q,R);
-    let newTri = new Konva.Shape({
-        sceneFunc:function(context,shape){
-            context.beginPath();
-            context.moveTo(startX,startY);
-            context.lineTo(startX+A,startY);
-            context.lineTo(startX+A,startY+B);
-            context.closePath();
-            context.fillStrokeShape(shape);
-        },
-        fill:fill,
-        stroke:stroke,
-        strokeWidth:1,
-        draggable:draggable,
+//current active shape
+let activeDrag;
+function rectGeN(startX,startY,stroke,fill,draggable,w,h){
+    let newRect = new Konva.Rect({
+        width:w,
+        height:h,
         x:startX,
         y:startY,
-        offset:{
-            x:arr[0],
-            y:arr[1]
-        }
-    });
-    layer.add(newTri);
-    draggableShapes.add(newTri);
+        stroke:stroke,
+        fill:fill,
+        id: 'square',
+        strokeWidth:1,
+        draggable:draggable,
+    })
+    draggableShapes.add(newRect)
 }
-//tw0 example triangles:
-TriangleGen(500,500,"black","white",true);
-TriangleGen(600,600,"black","white",true);
+function TriangleGen(startX, startY, stroke, fill, draggable, rotation, code) {
+    if (code == 0) {
+        if (rotation == 0) {
+            let P = [startX, startY];
+            let Q = [startX + A, startY];
+            let R = [startX + A, startY + A]
+            let arr = findCircumCenter(P, Q, R);
+            let newTri = new Konva.Shape({
+                sceneFunc: function (context, shape) {
+                    context.beginPath();
+                    context.moveTo(startX, startY);
+                    context.lineTo(startX + A, startY);
+                    context.lineTo(startX + A, startY + B);
+                    context.closePath();
+                    context.fillStrokeShape(shape);
+                },
+                fill: fill,
+                stroke: stroke,
+                strokeWidth: 1,
+                draggable: draggable,
+                id: 'triangle',
+                name: 0,
+                x: startX,
+                y: startY,
+                offset: {
+                    x: arr[0],
+                    y: arr[1]
+                }
+            });
+            //layer.add(newTri);
+            draggableShapes.add(newTri);
+        }
+    }
+    if (code == 1) {
+        let P;
+        let Q;
+        let R;
+        let arr;
+        switch (rotation) {
+            case 90:
+                P = [startX, startY];
+                Q = [startX, startY + A];
+                R = [startX - B, startY + A]
+                arr = findCircumCenter(P, Q, R);
+                break;
+            case 0:
+                P = [startX, startY];
+                Q = [startX + A, startY + B];
+                R = [startX + A, startY]
+                arr = findCircumCenter(P, Q, R);
+                break;
+            case 180:
+                P = [startX, startY];
+                Q = [startX, startY + B];
+                R = [startX + A, startY + B]
+                arr = findCircumCenter(P, Q, R);
+                break;
+            case 270:
+                P = [startX, startY];
+                Q = [startX + B, startY];
+                R = [startX, startY + A]
+                arr = findCircumCenter(P, Q, R);
+        }
+        let newTri = new Konva.Shape({
+            sceneFunc: function (context, shape) {
+                context.beginPath();
+                context.moveTo(P[0], P[1]);
+                context.lineTo(Q[0], Q[1]);
+                context.lineTo(R[0], R[1]);
+                context.closePath();
+                context.fillStrokeShape(shape);
+            },
+            fill: fill,
+            stroke: stroke,
+            strokeWidth: 1,
+            draggable: draggable,
+            id: 'triangle',
+            name:0,
+            x: startX,
+            y: startY,
+            offset: {
+                x: arr[0],
+                y: arr[1]
+            }
+        });
+        //layer.add(newTri);
+        staticShapes.push(newTri);
+        shapes.add(newTri);
+    }
+}
+//generating drag Shapes:
+function generateDragShapes(){
+    let sX = windowWidth*0.65;
+    let sY = drawHeight/8;
+    let variX = windowWidth*0.20 ;
+    let variY = drawHeight*0.30;
+    TriangleGen(sX,sY,"black","lightblue",true,0,0);
+    TriangleGen(sX+variX,sY,"black","lightblue",true,0,0);
+    TriangleGen(sX+variX,sY+variY,"black","lightblue",true,0,0);
+    TriangleGen(sX,sY+variY,"black","lightblue",true,0,0);
 
-layer.add(draggableShapes);
-stage.add(layer);
+    draggableShapes.children.forEach(function(e){
+        e.rotate(180);
+    })
+    rectGeN(sX+variX-((A+B)/4),sY+variY+A,"black","lightblue",true,A,A);
+    rectGeN(sX-((A+B)/4),sY+variY+A,"black","lightblue",true,B,B);
+}
+generateDragShapes();
+movable.add(draggableShapes);
+stationary.add(shapes);
+stage.add(movable);
+stage.add(stationary);
 
 //limiting drag area:
 draggableShapes.on("dragmove", (e) => {
@@ -75,35 +171,238 @@ draggableShapes.on("dragmove", (e) => {
     e.target.y(Math.min(e.target.getY(),drawHeight-200));
 });
 //rotation on dblclick:
-draggableShapes.on("dblclick",(e)=>{
+movable.on("dblclick",(e)=>{
     e.target.rotate(90);
+    if(e.target.getAttr('rotation') == 360)
+    {
+        e.target.setAttr('rotation', 0);
+    }
+})
+
+movable.on("click", e => {
+    console.log(e.target.getAttr('id'));
 })
 
 
-//TODO: rewrite this code!:
+shapes.on("click", e => {
+    console.log(e.target.getAttr('id'));
+})
 
-// JavaScript program to find the CIRCUMCENTER of a
-// triangle
+function drawTemplate(templateX,templateY) {
+    TriangleGen(templateX, templateY, "black", "black", false,270, 1)
+    TriangleGen(templateX, templateY, "black", "blue", false,90,  1)
+    TriangleGen(templateX + (B/2) + (A/2), templateY + (A/2) + (B/2), "black", "white", false,0,  1)
+    TriangleGen(templateX + (B/2) + (A/2), templateY + (A/2) + (B/2), "black", "black", false,180,  1)
+    let outlineSquare = new Konva.Rect({
+        x:templateX - B/2,
+        y:templateY - A/2,
+        width:A+B,
+        height:A+B,
+        opacity:1,
+        stroke:"black",
+        strokeWidth:1,
+    })
+    let squareBig = new Konva.Rect({
+        x: templateX - (B/2),
+        y: templateY + (A/2),
+        width: B,
+        height: B,
+        fill: 'red',
+        stroke: 'black',
+        id: 'square',
+        strokeWidth: 1,
+        draggable: false,
+        name:0,
+    });
+    let squareSmall = new Konva.Rect({
+        x: templateX + (B/2),
+        y: templateY - (A/2),
+        width: A,
+        height: A,
+        fill: 'red',
+        stroke: 'black',
+        id: 'square',
+        strokeWidth: 1,
+        draggable: false,
+        name:0,
+    });
+    shapes.add(squareBig)
+    shapes.add(squareSmall)
+    shapes.add(outlineSquare)
+    staticShapes.push(squareSmall)
+    staticShapes.push(squareBig)
+    staticShapes.push(outlineSquare)
+    console.log(staticShapes[0].getX(), staticShapes[0].getY())
+}
 
-// This pair is used to store the X and Y
-// coordinate of a point respectively
-// #define pdd pair<double, double>
+drawTemplate(windowWidth/4,drawHeight/2-((A+B)/2)-20)
 
-// Function to find the line given two points
-function lineFromPoints(P, Q) {
+movable.zIndex(1);
+stationary.zIndex(0);
+
+//setting the opacity of all shapes to 0:
+for(i in staticShapes){
+    staticShapes[i].opacity(1);
+}
+
+let tempLayer = new Konva.Layer();
+stage.add(tempLayer);
+let previousShape;
+
+
+stage.on('dragstart', function (e) {
+    e.target.moveTo(tempLayer);
+    movable.draw();
+});
+
+stage.on('dragmove', function (evt) {
+    activeDrag = evt.target
+    var pos = stage.getPointerPosition();
+    var shape = movable.getIntersection(pos);
+    if (previousShape && shape) {
+        if (previousShape !== shape) {
+            // leave from old targer
+            previousShape.fire(
+                'dragleave',
+                {
+                    evt: evt.evt,
+                },
+                true
+            );
+
+            // enter new targer
+            shape.fire(
+                'dragenter',
+                {
+                    evt: evt.evt,
+                },
+                true
+            );
+            previousShape = shape;
+        } else {
+            previousShape.fire(
+                'dragover',
+                {
+                    evt: evt.evt,
+                },
+                true
+            );
+        }
+    } else if (!previousShape && shape) {
+        previousShape = shape;
+        shape.fire(
+            'dragenter',
+            {
+                evt: evt.evt,
+            },
+            true
+        );
+    } else if (previousShape && !shape) {
+        previousShape.fire(
+            'dragleave',
+            {
+                evt: evt.evt,
+            },
+            true
+        );
+        previousShape = undefined;
+    }
+
+    staticShapes.forEach(e =>{
+        if((e.getX()+50 >= activeDrag.getX() && e.getX()-50 <= activeDrag.getX()) && (e.getY()+50 >= activeDrag.getY() && e.getY()-50 <= activeDrag.getY()) && e.getAttr('id') === 'square' && activeDrag.getAttr('id') === 'square' && ((e.getAttr('width') === activeDrag.getAttr('width'))))
+        {
+            e.setAttr('fill', 'green');
+        }
+        if((e.getX()+50 >= activeDrag.getX() && e.getX()-50 <= activeDrag.getX()) && (e.getY()+50 >= activeDrag.getY() && e.getY()-50 <= activeDrag.getY()) && e.getAttr('id') == activeDrag.getAttr('rotation') && activeDrag.getAttr('id') != 'square')
+        {
+            e.setAttr('fill', 'green');
+        }
+    })
+
+});
+stage.on('dragend', function (e) {
+    var pos = stage.getPointerPosition();
+    var shape = movable.getIntersection(pos);
+    if (shape) {
+        previousShape.fire(
+            'drop',
+            {
+                evt: e.evt,
+            },
+            true
+        );
+    }
+    previousShape = undefined;
+    e.target.moveTo(movable);
+
+    staticShapes.forEach(e =>{
+        if((e.getX()+50 >= activeDrag.getX() && e.getX()-50 <= activeDrag.getX()) && (e.getY()+50 >= activeDrag.getY() && e.getY()-50 <= activeDrag.getY()) && e.getAttr('id') == activeDrag.getAttr('rotation') && e.getAttribute(name) != 1)
+        {
+            if(activeDrag.getAttr('rotation') == 0)
+            {
+                activeDrag.setAttr('x', e.getX());
+                activeDrag.setAttr('y', e.getY() - (B-A)/2);
+                activeDrag.setAttr('draggable', false);
+                e.setAttribute(name) = 1;
+                activeDrag.moveTo(stationary)
+            }
+            if(activeDrag.getAttr('rotation') == 90)
+            {
+                activeDrag.setAttr('x', e.getX() + (B-A)/2);
+                activeDrag.setAttr('y', e.getY());
+                activeDrag.setAttr('draggable', false);
+                e.setAttribute(name) = 1;
+                activeDrag.moveTo(stationary);
+            }
+            if(activeDrag.getAttr('rotation') == 180)
+            {
+                activeDrag.setAttr('x', e.getX());
+                activeDrag.setAttr('y', e.getY() + (B-A)/2);
+                activeDrag.setAttr('draggable', false);
+                e.setAttribute(name) = 1;
+                activeDrag.moveTo(stationary)
+            }
+            if(activeDrag.getAttr('rotation') == 270)
+            {
+                activeDrag.setAttr('x', e.getX() - (B-A)/2);
+                activeDrag.setAttr('y', e.getY());
+                activeDrag.setAttr('draggable', false);
+                e.setAttribute(name) = 1;
+                activeDrag.moveTo(stationary)
+            }
+        }
+        if((e.getX()+50 >= activeDrag.getX() && e.getX()-50 <= activeDrag.getX()) && (e.getY()+50 >= activeDrag.getY() && e.getY()-50 <= activeDrag.getY()) && e.getAttr('id') == 'square' && activeDrag.getAttr('id') == 'square' && ((e.getAttr('width') == activeDrag.getAttr('width'))) && e.getAttribute(name) != 1)
+        {
+            activeDrag.setAttr('x', e.getX());
+            activeDrag.setAttr('y', e.getY());
+            activeDrag.setAttr('draggable', false)
+            e.setAttribute(name) = 1;
+            activeDrag.moveTo(stationary)
+        }
+    })
+});
+stage.on('drop', e => {
+
+})
+stage.on('dragover', e => {
+    /*if((e.target.getAttribute('id') === activeDrag.getAttribute('id')) && ((e.target.getAttribute('rotation') === activeDrag.getAttribute('rotation')))){
+        activeDrag.setAttribute('fill', 'green');
+    }*/
+
+    console.log("suka");
+})
+
+//maths Functions to find Circumcenter:
+function line(P, Q) {
     let a = Q[1] - P[1];
     let b = P[0] - Q[0];
     let c = a * (P[0]) + b * (P[1]);
-    return [a, b, c];
+    return [a,b,c];
 }
 
-// Function which converts the input line to its
-// perpendicular bisector. It also inputs the points
-// whose mid-point lies on the bisector
-function perpendicularBisectorFromLine(P, Q, a, b, c) {
+function PBisect(P, Q, a, b, c) {
     let mid_point = [(P[0] + Q[0]) / 2, (P[1] + Q[1]) / 2];
 
-    // c = -bx + ay
     c = -b * (mid_point[0]) + a * (mid_point[1]);
 
     let temp = a;
@@ -112,12 +411,10 @@ function perpendicularBisectorFromLine(P, Q, a, b, c) {
     return [a, b, c];
 }
 
-// Returns the intersection point of two lines
-function lineLineIntersection(a1, b1, c1, a2, b2, c2) {
+function linelineIntersect(a1, b1, c1, a2, b2, c2) {
     let determinant = a1 * b2 - a2 * b1;
     if (determinant == 0) {
-        // The lines are parallel. This is simplified
-        // by returning a pair of FLT_MAX
+
         return [(10.0) ** 19, (10.0) ** 19];
     } else {
         let x = (b2 * c1 - b1 * c2) / determinant;
@@ -127,38 +424,26 @@ function lineLineIntersection(a1, b1, c1, a2, b2, c2) {
 }
 
 function findCircumCenter(P, Q, R) {
-    // Line PQ is represented as ax + by = c
-    let PQ_line = lineFromPoints(P, Q);
-    let a = PQ_line[0];
-    let b = PQ_line[1];
-    let c = PQ_line[2];
+    let PQ = line(P, Q);
+    let a = PQ[0];
+    let b = PQ[1];
+    let c = PQ[2];
 
-    // Line QR is represented as ex + fy = g
-    let QR_line = lineFromPoints(Q, R);
-    let e = QR_line[0];
-    let f = QR_line[1];
-    let g = QR_line[2];
+    let QR = line(Q, R);
+    let e = QR[0];
+    let f = QR[1];
+    let g = QR[2];
 
-    // Converting lines PQ and QR to perpendicular
-    // vbisectors. After this, L = ax + by = c
-    // M = ex + fy = g
-    let PQ_perpendicular = perpendicularBisectorFromLine(P, Q, a, b, c);
-    a = PQ_perpendicular[0];
-    b = PQ_perpendicular[1];
-    c = PQ_perpendicular[2];
+    let PQ_Perp = PBisect(P, Q, a, b, c);
+    a = PQ_Perp[0];
+    b = PQ_Perp[1];
+    c = PQ_Perp[2];
 
-    let QR_perpendicular = perpendicularBisectorFromLine(Q, R, e, f, g);
-    e = QR_perpendicular[0];
-    f = QR_perpendicular[1];
-    g = QR_perpendicular[2];
+    let QR_Perp = PBisect(Q, R, e, f, g);
+    e = QR_Perp[0];
+    f = QR_Perp[1];
+    g = QR_Perp[2];
 
-    // The point of intersection of L and M gives
-    // the circumcenter
-    return lineLineIntersection(a, b, c, e, f, g);
+    return linelineIntersect(a, b, c, e, f, g);
 
 }
-
-// Driver code.
-
-
-// The code is contributed by Gautam goel (gautamgoel962)
