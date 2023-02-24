5af6d9519b0928f73eb4273803c3b24cacdfce0f
Luggamugga <124262306+Luggamugga@users.noreply.github.com>
2023-02-24 13:47:11 +0100

    Add files via upload

--- shapes.php
+++ shapes.php
@@ -1,18 +1,51 @@
 <html>
-<head><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
     <script src="https://unpkg.com/konva@8/konva.min.js"></script>
-<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div id="textBox"><div id="textContainer">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet</div>
-    <div id="nextButtContainer"><button type="button" id="nextButt"><a href="shapes2.php?a=<?=$_GET['a']?>&b=<?=$_GET['b']?>">Next Level</a></button></div>
+<div id="keyContainer">
+    <table>
+        <tr>
+            <th>Color:</th>
+            <th>Sidelength(s):</th>
+        </tr>
+        <tr>
+            <td style="background-color:lightblue"></td>
+            <td><span id="triSidelengths">a</span></td>
+        </tr>
+        <tr>
+            <td style="background-color:#89a1c4 "></td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td style="background-color:#2d4260"></td>
+            <td>B</td>
+        </tr>
+    </table>
+</div>
+<div id="textBox">
+    <div id="textContainer">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet
+    </div>
+
+        <div id="hintButtContainer">
+            <button id="hint">Get Hint</button>
+        </div>
+
+
+        <div id="nextButtContainer">
+            <button type="button" id="nextButt"><a href="shapes2.php?a=<?= $_GET['a'] ?>&b=<?= $_GET['b'] ?>">Next
+                    Level</a>
+            </button>
+        </div>
 
 </div>
-
 <div id="draw"></div>
-
 
 
 <script src="shapesApp.js"></script>
